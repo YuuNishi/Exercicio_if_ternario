22ea4cb26e27f0c3e4a11af182362cea4e3b6fd3
Yumi Mariane Nishiguchi <yumi.nishiguchi@programmers.com.br>
2022-06-06 10:45:13 -0300

    Exercicio com if e operador ternario

---
+++ if_ternario/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite uma nota");
+float nota = float.Parse(Console.ReadLine());
+
+if(nota >=8)
+{
+    Console.WriteLine("Aprovado");
+}else if(nota>=5 && nota < 8)
+{
+    Console.WriteLine("Recuperacao");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
+//////////////ternario
+String situacao =nota >= 8 ? "Aprovado" : "Reprovado";
+Console.WriteLine(situacao);
